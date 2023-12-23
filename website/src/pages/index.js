ca75c885bc7350a2ca99580c86b6f3f428290ece
Nitya Narasimhan <n.narasimhan@gmail.com>
2023-12-23 16:17:21 -0500

    Interim

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,6 +5,122 @@ import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
+
+
+const FeatureList = [
+  {
+    title: 'Responsbile AI Principles',
+    tag: 'Concepts',
+    tagColor: 'badge badge--primary',
+    img: "img/landing/rai-collection-1.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Responsible AI Dashboard',
+    tag: 'Concepts',
+    tagColor: 'badge badge--primary',
+    img: "img/landing/rai-collection-2.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 3',
+    tag: 'Tabular Model',
+    tagColor: 'badge badge--warning',
+    img: "img/landing/rai-collection-3.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 4',
+    tag: 'Tabular Model',
+    tagColor: 'badge badge--warning',
+    img: "img/landing/rai-collection-4.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 5',
+    tag: 'Tabular Model',
+    tagColor: 'badge badge--warning',
+    img: "img/landing/rai-collection-5.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 6',
+    tag: 'Text Model',
+    tagColor: 'badge badge--success',
+    img: "img/landing/rai-collection-6.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 7',
+    tag: 'Text Model',
+    tagColor: 'badge badge--success',
+    img: "img/landing/rai-collection-7.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 8',
+    tag: 'Text Model',
+    tagColor: 'badge badge--success',
+    img: "img/landing/rai-collection-8.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 9',
+    tag: 'Vision Model',
+    tagColor: 'badge badge--danger',
+    img: "img/landing/rai-collection-9.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+  {
+    title: 'Exercise 10',
+    tag: 'Vision Model',
+    tagColor: 'badge badge--danger',
+    img: "img/landing/rai-collection-10.jpeg",
+    description: (
+      <>
+        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
+      </>
+    ),
+  },
+];
+
+
 function PageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -43,9 +159,9 @@ function PageFeatures() {
   );
 }
 
-function Feature({img, title, description }) {
+function Feature({img, title, description, tag, tagColor}) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx('col col--3')}>
       <div className="card-demo">
         <div className="card">
           <div className="card__image">
@@ -58,10 +174,11 @@ function Feature({img, title, description }) {
             <h4>{title}</h4>
             <small>
               {description}
+              <span className={tagColor}>{tag}</span>
             </small>
           </div>
           <div className="card__footer">
-            <button className="button button--primary button--block">Visit</button>
+            <button className="button disabled button--info button--block">Open Notebook</button>
           </div>
         </div>
       </div>
@@ -69,106 +186,13 @@ function Feature({img, title, description }) {
   );
 }
 
-const FeatureList = [
-  {
-    title: 'Exercise 1',
-    img: "img/landing/rai-collection-1.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 2',
-    img: "img/landing/rai-collection-2.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 3',
-    img: "img/landing/rai-collection-3.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 4',
-    img: "img/landing/rai-collection-4.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 5',
-    img: "img/landing/rai-collection-5.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 6',
-    img: "img/landing/rai-collection-6.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 7',
-    img: "img/landing/rai-collection-7.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 8',
-    img: "img/landing/rai-collection-8.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 9',
-    img: "img/landing/rai-collection-9.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-  {
-    title: 'Exercise 10',
-    img: "img/landing/rai-collection-10.jpeg",
-    description: (
-      <>
-        This notebook will walk you through the process of ..... with the Responsible AI Toolkit.
-      </>
-    ),
-  },
-];
-
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`#30DaysOf Responsible AI`}
+      description="Learn Responsible AI Principles and see them in practice using open-source Responsible AI Toolbox components with a Responsible AI Dashboard.">
       <PageHeader />
       <main>
         <PageFeatures />
